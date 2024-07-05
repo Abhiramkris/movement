fc42852355c268b0f5b1fa5bb14f5df34d291e6e
abhiram krishna <kabhiram67@gmail.com>
2024-07-05 16:29:51 +0530

    first commit

--- app.js
+++ app.js
@@ -59,10 +59,11 @@ const client = twilio(accountSid, authToken);
 
 const db = mysql.createConnection({
     host: process.env.DB_HOST,
-    port: process.env.PORT || 3306,
+    port: 17422,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    timeout: 60000
 });
 
 db.connect(err => {
