cdd5ca49822a16e0d8fb60c13e68d848d10a4294
abhiram krishna <82257091+Abhiramkris@users.noreply.github.com>
2024-07-12 22:13:16 +0530

    Update app.js

--- app.js
+++ app.js
@@ -62,8 +62,7 @@ const db = mysql.createConnection({
     port: 23642,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    timeout: 60000
+    database: process.env.DB_NAME
 });
 
 db.connect(err => {
